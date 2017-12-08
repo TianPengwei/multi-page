d69f5d53d290154a40ffde170cc20919d7415f46
SC-201711141442\Administrator <736101853@qq.com>
2017-12-08 10:25:39 +0800

    修改

--- src/assets/js/es6.js
+++ src/assets/js/es6.js
@@ -14,5 +14,63 @@ require('../css/pages/es6.scss');
 }
 
 {
+    //class
+    console.log('----------------class-------------------');
+    class Coder {
+        name(val) {
+            return val;
+        }
+        skill(val) {
+            console.log(this.name('肥仔')+ ':Skill-' +val);
+        }
+    };
+    let tpw = new Coder;
+    tpw.skill('前端');
+}
+
+{
+    //proxy
+    console.log('----------------proxy-------------------');
+    let o = {
+        add : function(val) {
+            return val + 100;
+        },
+        name : '田鹏伟'
+    };
+    let pro = new Proxy({
+        add : function(val) {
+            return val + 100;
+        },
+        name : '田鹏伟'
+    },{
+        get : function(target,key,property) {
+            console.log('come in get');
+            return target[key];
+        }
+    });
+    console.log(pro.name);
+}
+
+{
+    //map
+    console.log('----------------map-------------------');
+    const o = {
+        name : '田鹏伟',
+        age : 20
+    };
+    const arr = [1,2,3];
+    const m = new Map();
+    m.set(o,'人');
+    m.set(arr,o);
+    m.delete(arr);
+    console.log(m);
+}
+
+{
     const dom = document.getElementById('J_es6');
+    let setArr = new Set(['田','鹏','伟','田']);
+    console.log(setArr);  //Set(3) {'田','鹏','伟'}
+    console.log(Array.isArray(setArr));  //false
+    for(let item of setArr) console.log(item);
+    setArr.forEach((val)=>console.log(val));
 }
